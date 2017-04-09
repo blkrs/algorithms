06275412707ab6ab5a3b93db46219abbe53700ca
Krzysztof Balka <krzysztof.balka@gmail.com>
2017-04-09 18:22:33 +0200

    Reading vector from command line args

--- sum_in_vector/find_sum.c
+++ sum_in_vector/find_sum.c
@@ -12,7 +12,7 @@ int find_sum(int *v, int l, int sum) {
   int left = 0;
   int right = l - 1;
   while (left < right) {
-    printf("l = %d, r = %d\n", left, right);
+    printf("l = %d, r = %d, %d %d\n", left, right, v[left], v[right]);
     if (v[left] + v[right] == sum) {
       printf("%d %d\n", v[left], v[right]);
       return 1;
@@ -25,9 +25,22 @@ int find_sum(int *v, int l, int sum) {
   return 0;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+ int i;
+ if (argc == 1) {
  find_sum(va,7,9);
  find_sum(vb,4,9); 
  find_sum(vc,4,9); 
+ } else {
+   int l = argc - 2;
+   int sum = atoi(argv[argc-1],10);
+   printf("Searching for sum: %d\n", sum);
+   int * v = malloc(sizeof(int) * (l));
+   for (i = 0; i < l; ++i) {
+     v[i] = atoi(argv[i+1], 10);
+   }
+   find_sum(v, l, sum);
+   free(v);
+ }
  return 0;
 }
